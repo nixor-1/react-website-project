baf8dc546605adf8dafa1d88db531f41a548b85c
Nick Jørgensen <nickjoergensen1@gmail.com>
2026-06-01 16:23:39 +0200

    Slight visual adjustment to homepage when viewd on small screens

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -8,17 +8,19 @@ const HomePage = () => {
   const { t } = useTranslation();
 
   const spacingStyles =
-    "pb-0 border-b border-color-primary md:pb-6 md:border-b-0";
+    "pb-0 border-b border-color-primary md:pb-6 md:border-t-0 md:border-b-0";
 
   return (
     <PageMarginsNew pageMarginsWidth="2" isScrollable={true}>
-      <div className={`${spacingStyles} border-b-1 border-color-primary`}>
+      <div
+        className={`${spacingStyles} border-t border-b-2 border-color-primary`}
+      >
         <TextBox
           textBoxTitle={t("home-page.welcome.title")}
           textBoxContent={<WelcomeTextBody />}
         />
       </div>
-      <div className={`${spacingStyles} border-b-1 border-color-primary`}>
+      <div className={`${spacingStyles} border-b-2 border-color-primary`}>
         <TextBox
           textBoxTitle={t("home-page.status.title")}
           textBoxContent={<WebsiteStatusTextBody />}
